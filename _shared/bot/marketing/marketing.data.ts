34a9156d6386087561bbdbbb0bae86527181614b
erodion4776 <maelizaenterprises@gmail.com>
2026-08-10 12:47:15 +0100

    Re-export marketing demo data from demo.data.ts

---
+++ _shared/bot/marketing/marketing.data.ts
@@ -0,0 +1,10 @@
+// Just re-export from the existing demo data
+// Move your demo.data.ts here
+export {
+  DEMO_SCHOOL,
+  DEMO_ATTENDANCE,
+  DEMO_FEES,
+  DEMO_PICKUP,
+  DEMO_REPORTS,
+  SETUP_FEE_TIERS,
+} from '../../onboarding/../bot/marketing/demo.data.ts';
