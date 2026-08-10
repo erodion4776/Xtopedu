35e320947b67038c514be70c8909fa261d996804
erodion4776 <maelizaenterprises@gmail.com>
2026-08-10 20:29:19 +0100

    Add loading skeleton component for leads

---
+++ dashboard/app/leads/loading.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function LeadsLoading() {
+  return (
+    <div className="space-y-4">
+      <Skeleton className="h-10 w-full" />
+      <div className="flex gap-2">
+        {[...Array(5)].map((_, i) => (
+          <Skeleton
+            key={i}
+            className="h-7 w-20 rounded-full"
+          />
+        ))}
+      </div>
+      <div className="space-y-3">
+        {[...Array(5)].map((_, i) => (
+          <Skeleton key={i} className="h-24 rounded-xl" />
+        ))}
+      </div>
+    </div>
+  );
+}l
